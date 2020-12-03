392e83377cc07c780af3cdabc93e5068abdbc9c4
Lasse Meyer <meyer.lasse@gmail.com>
2020-12-03 20:47:21 +0100

    Update both scripts

--- rpi3-disable-leds.sh
+++ rpi3-disable-leds.sh
@@ -2,7 +2,7 @@
 # Disable all LEDs on a Raspberry Pi 3
 
 # Disable Ethernet LEDs
-/usr/local/bin/lan951x-led-ctl --fdx=0 --lnk=0 --spd=0
+lan951x-led-ctl --fdx=0 --lnk=0 --spd=0
 
 echo none > /sys/class/leds/led0/trigger
 echo 0 > /sys/class/leds/led0/brightness
--- rpi3-pco.sh
+++ rpi3-pco.sh
@@ -9,68 +9,22 @@ if (( "$EUID" != 0 )); then
 	exit 1
 fi
 
+# Disable Wifi, Bluetooth, Audio, I2C, I2S, SPI
+ALLOFIT_DISABLE="dtoverlay=pi3-disable-wifi,pi3-disable-bt,audio=off,i2c=off,i2s=off,spi=off"
+ALLOFIT_DISABLE_CMT="# Disable Wifi, Bluetooth, Audio, I2C, I2S, SPI"
+
+sed -i "/$ALLOFIT_DISABLE/d" "$BOOT_CFG_FILE"
+sed -i "/$ALLOFIT_DISABLE_CMT/d" "$BOOT_CFG_FILE"
+
+echo "" >> "$BOOT_CFG_FILE"
+echo "$ALLOFIT_DISABLE_CMT" >> "$BOOT_CFG_FILE"
+echo "$ALLOFIT_DISABLE" >> "$BOOT_CFG_FILE"
+
 # Disable HDMI
 /opt/vc/bin/tvservice -o
 
-# Disable Wi-Fi
-WIFI_DISABLE="dtoverlay=pi3-disable-wifi"
-WIFI_DISABLE_CMT="# Disable Wifi"
-
-systemctl disable raspberrypi-net-mods.service
-systemctl disable wifi-country.service
-systemctl disable wpa_supplicant.service
-
-sed -i "/$WIFI_DISABLE/d" "$BOOT_CFG_FILE"
-sed -i "/$WIFI_DISABLE_CMT/d" "$BOOT_CFG_FILE"
-
-echo "" >> "$BOOT_CFG_FILE"
-echo "$WIFI_DISABLE_CMT" >> "$BOOT_CFG_FILE"
-echo "$WIFI_DISABLE" >> "$BOOT_CFG_FILE"
-
-# Disable Bluetooth
-BT_DISABLE="dtoverlay=pi3-disable-bt"
-BT_DISABLE_CMT="# Disable Bluetooth"
-
-systemctl disable bluetooth.service
-systemctl disable hciuart.service
-
-sed -i "/$BT_DISABLE/d" "$BOOT_CFG_FILE"
-sed -i "/$BT_DISABLE_CMT/d" "$BOOT_CFG_FILE"
-
-echo "" >> "$BOOT_CFG_FILE"
-echo "$BT_DISABLE_CMT" >> "$BOOT_CFG_FILE"
-echo "$BT_DISABLE" >> "$BOOT_CFG_FILE"
-
 # Disable USB & Ethernet
 #echo '1-1' | tee /sys/bus/usb/drivers/usb/unbind
-
-# Disable Activity LED
-#LEDACT_DISABLE_1="dtparam=act_led_trigger"
-#LEDACT_DISABLE_2="dtparam=act_led_activelow"
-#LEDACT_DISABLE_CMT="# Disable Activity LED"
-
-#sed -i "/$LEDACT_DISABLE_1/d" "$BOOT_CFG_FILE"
-#sed -i "/$LEDACT_DISABLE_2/d" "$BOOT_CFG_FILE"
-#sed -i "/$LEDACT_DISABLE_CMT/d" "$BOOT_CFG_FILE"
-
-#echo "" >> "$BOOT_CFG_FILE"
-#echo "$LEDACT_DISABLE_CMT" >> "$BOOT_CFG_FILE"
-#echo "$LEDACT_DISABLE_1=none" >> "$BOOT_CFG_FILE"
-#echo "$LEDACT_DISABLE_2=off" >> "$BOOT_CFG_FILE"
-
-# Disable Power LED
-#LEDPWR_DISABLE_1="dtparam=pwr_led_trigger"
-#LEDPWR_DISABLE_2="dtparam=pwr_led_activelow"
-#LEDPWR_DISABLE_CMT="# Disable Power LED"
-
-#sed -i "/$LEDPWR_DISABLE_1/d" "$BOOT_CFG_FILE"
-#sed -i "/$LEDPWR_DISABLE_2/d" "$BOOT_CFG_FILE"
-#sed -i "/$LEDPWR_DISABLE_CMT/d" "$BOOT_CFG_FILE"
-
-#echo "" >> "$BOOT_CFG_FILE"
-#echo "$LEDPWR_DISABLE_CMT" >> "$BOOT_CFG_FILE"
-#echo "$LEDPWR_DISABLE_1=none" >> "$BOOT_CFG_FILE"
-#echo "$LEDPWR_DISABLE_2=off" >> "$BOOT_CFG_FILE"
 
 # Disable Power and Activity LEDs on Raspbian Buster
 if ! service rpi3-disable-leds status; then
@@ -100,53 +54,28 @@ mv "$ETH_DIS_SRV" /etc/systemd/system
 systemctl enable "$ETH_DIS_SRV"
 systemctl start "$ETH_DIS_SRV"
 
-# Disable audio
-AUDIO_DISABLE="dtparam=audio"
-AUDIO_DISABLE_CMT="# Disable audio (snd_bcm2835)"
-
-sed -i "/$AUDIO_DISABLE/d" "$BOOT_CFG_FILE"
-sed -i "/$AUDIO_DISABLE_CMT/d" "$BOOT_CFG_FILE"
-
-echo "" >> "$BOOT_CFG_FILE"
-echo "$AUDIO_DISABLE_CMT" >> "$BOOT_CFG_FILE"
-echo "$AUDIO_DISABLE=off" >> "$BOOT_CFG_FILE"
-
-# Disable I2C
-I2C_DISABLE="dtparam=i2c_arm"
-I2C_DISABLE_CMT="# Disable I2C"
-
-sed -i "/$I2C_DISABLE/d" "$BOOT_CFG_FILE"
-sed -i "/$I2C_DISABLE_CMT/d" "$BOOT_CFG_FILE"
-
-echo "" >> "$BOOT_CFG_FILE"
-echo "$I2C_DISABLE_CMT" >> "$BOOT_CFG_FILE"
-echo "$I2C_DISABLE=off" >> "$BOOT_CFG_FILE"
-
-# Disable SPI
-SPI_DISABLE="dtparam=spi"
-SPI_DISABLE_CMT="# Disable SPI"
-
-sed -i "/$SPI_DISABLE/d" "$BOOT_CFG_FILE"
-sed -i "/$SPI_DISABLE_CMT/d" "$BOOT_CFG_FILE"
-
-echo "" >> "$BOOT_CFG_FILE"
-echo "$SPI_DISABLE_CMT" >> "$BOOT_CFG_FILE"
-echo "$SPI_DISABLE=off" >> "$BOOT_CFG_FILE"
-
 # Set Ethernet speed to 10 Mb/s
 
 # Underclock CPU
 # No energy saving if model 3 or higher --> ARM optimizations good enough
 
 # Disable other unnecessary services & daemons
+systemctl disable alsa-utils.service
+service alsa-utils stop
 systemctl disable avahi-daemon.service
+service avahi-daemon stop
+systemctl disable bluetooth.service
+service bluetooth stop
 systemctl disable display-manager.service
+service display-manager stop
 systemctl disable gldriver-test.service
+service gldriver-test stop
 systemctl disable keyboard-setup.service
+service keyboard-setup stop
 systemctl disable lightdm.service
+service lightdm stop
 systemctl disable rpi-display-backlight.service
-systemctl disable triggerhappy.service
-systemctl disable triggerhappy.socket
+service rpi-display-backlight stop
 
 # Boot optimization
 # - Add noarp to /etc/dhcpcd.conf
